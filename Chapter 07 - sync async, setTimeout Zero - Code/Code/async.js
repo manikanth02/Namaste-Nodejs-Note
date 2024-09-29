3e1dde30d30161b8b747b1eedcae645ad1f2a30d
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-09-29 18:02:18 +0530

    Topic: Crypto module and thread blocking with Sync function:bulb:

--- Chapter 07 - sync async, setTimeout Zero - Code/Code/async.js
+++ Chapter 07 - sync async, setTimeout Zero - Code/Code/async.js
@@ -6,6 +6,8 @@ console.log("Asynchronous Javascript")
 var a = 5;
 var b = 10;
 
+fs.readFileSync("./file.txt", "utf8");
+console.log("This will execute only after reading the file")
 
 https.get("https://dummyjson.com/products/1", (res) => {
     console.log("data fetch successfully")
@@ -15,6 +17,10 @@ setTimeout(() => {
     console.log("Execute it after 5 seconds")
 }, 5000)
 
+//Synchronous function
+//it will block the main thread
+
+//asynchrous functions
 fs.readFile("./file.txt", "utf-8", (err, data) => {
     console.log("file data:" + data)
 })
