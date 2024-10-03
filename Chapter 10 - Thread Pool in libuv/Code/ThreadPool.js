3be7bbe7a7b78fa87b1af103cd55540d95781b1d
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-10-03 18:03:18 +0530

    Update: Manually Thread Pool vary

--- Chapter 10 - Thread Pool in libuv/Code/ThreadPool.js
+++ Chapter 10 - Thread Pool in libuv/Code/ThreadPool.js
@@ -1,9 +1,11 @@
 const fs = require("fs");
 const crypto = require("crypto")
 
-fs.readFile("./file.txt", "utf-8", (err, data) => {
-    console.log(data);
-})
+process.env.UV_THREADPOOL_SIZE = 5;
+
+// fs.readFile("./file.txt", "utf-8", (err, data) => {
+//     console.log(data);
+// })
 //The 4 keys will generate first and after that remaining one will be generated after some time because nodejs has only 4 threads at a time 
 crypto.pbkdf2("Akshad", "salt", 500000, 10, "sha512", (err, key) => {
     console.log("1st key generated below:")
