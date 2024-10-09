e0de0033eae0b03c287370c11fe5a40e8cbd55ca
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2024-10-09 17:46:37 +0530

    Code: Database Connection and CRUD Operation :rocket:

--- Chapter 13 - Creating a database & mongodb/Code/db.js
+++ Chapter 13 - Creating a database & mongodb/Code/db.js
@@ -1,1 +1,50 @@
-const URI="mongodb+srv://akshadsantoshjaiswal:rFw7NSxncTpSc7Bq@cluster0.yulixmn.mongodb.net/"
+const { MongoClient } = require("mongodb");
+const { ObjectId } = require("mongodb");
+
+//connection URL
+const url = "mongodb+srv://akshadsantoshjaiswal:rFw7NSxncTpSc7Bq@cluster0.yulixmn.mongodb.net/"
+const client = new MongoClient(url);
+
+const dbname = "NamsteNodejs"
+
+async function main() {
+    await client.connect();
+    console.log("Database connected successfully")
+    const db = client.db(dbname);
+    const collection = db.collection("User")
+
+    const data = {
+        firstname: "Akshad",
+        lastname: "Jaiswal",
+        city: "Pune",
+        phoneNumber: "88526587",
+    }
+
+    
+    // Create
+    // const insertData = await collection.insertMany([data])
+    // console.log("data inserted = ", insertData)
+
+
+    // Update
+    // const updateData = await collection.updateOne({ _id: new ObjectId('67066d6a3be8f41630d5dae4') }, { $set: { firstname: "Mint" } })
+    // console.log("Updated document ", updateData)
+
+
+    //Read 
+    const findData = await collection.find({}).toArray();
+    console.log("All data :", findData)
+
+    //delete
+    // const deletedata = await collection.deleteOne({ _id: new ObjectId('670668562c6bd11e25050c13') })
+    // console.log("deleted data=>", deletedata)
+
+
+    return 'done'
+
+
+}
+
+main().then(console.log)
+    .catch(console.error)
+    .finally(() => client.close());
