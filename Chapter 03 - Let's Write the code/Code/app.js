7d90fb4f60626c03363578d1b02564c280816376
Akshad Jaiswal <akshadsantoshjaiswal@gmail.com>
2025-01-17 20:30:12 +0530

    Update app.js

--- Chapter 03 - Let's Write the code/Code/app.js
+++ Chapter 03 - Let's Write the code/Code/app.js
@@ -7,7 +7,7 @@ console.log(name);
 console.log(c);
 
 // console.log(global)
-
 // The globalThis global property contains the global this value, which is usually akin to the global object.
+
 console.log(globalThis)
 console.log(globalThis=== global)
